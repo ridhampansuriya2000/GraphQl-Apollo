b1ea8d9747c5cd23b69df3de9f94e4c170df684b
ridhampansuriya <ridhampansureiya200@gmail.com>
2022-09-05 19:01:17 +0530

    add mutation with Live DB

--- resolvers.js
+++ resolvers.js
@@ -1,7 +1,13 @@
 import {quotes, users} from "./fakedb.js";
+import mongoose from "mongoose";
+
 
 /*-------------------------Create Random ID-----------------------------*/
 import {randomBytes} from 'crypto';
+
+const User = mongoose.model("User")
+import bcrypt from 'bcryptjs';
+
 
 const resolvers = {
     Query:{
@@ -22,13 +28,18 @@ const resolvers = {
              })
              return users.find(user => user._id == _id);
          },
-         signupUser:(_,{userNew})=>{
-             const _id = randomBytes(5).toString("hex")
-             users.push({
-                 _id,
-                 ...userNew
-             })
-             return users.find(user => user._id == _id);
+         signupUser: async (_,{userNew})=>{
+            const user =  await User.findOne({email : userNew.email});
+             if(user){
+                 throw new Error("User already exist with that email")
+             }
+          const hashedPassword =  await bcrypt.hash(userNew.password,12);
+
+            const newUser = new User({
+                 ...user,
+                 password : hashedPassword
+             });
+             return await newUser.save();
          }
      }
 }
